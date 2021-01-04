71ffac04a517849a5e02121f534263af7cb9127d
raulng9 <salprez4@gmail.com>
2021-01-04 13:26:19 +0100

    New address change

--- Main.py
+++ Main.py
@@ -21,11 +21,10 @@ import os
 
 app = Flask(__name__)
 app.config['SERVER_NAME']='https://checkers-vs-computer.herokuapp.com:5000'
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 #app.config['DEBUG'] = True
 #app.run(host="0.0.0.0",port=5000)
-# with app.app_context(), app.test_request_context():
-#     url = url_for('templates')
-port = int(os.environ.get("PORT", 5000))
 
 check = CheckersGame()
 check.play()
